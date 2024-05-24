62facfd7e1953d60615fa564e3b9fa657742582a
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-24 22:40:49 +0300

    Add web services implementation for getting all users for Retrofit

--- lib/web_services.dart
+++ lib/web_services.dart
@@ -1,3 +1,12 @@
+import 'package:api_project/users.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+part 'web_services.g.dart';
+
+@RestApi(baseUrl: "https://gorest.co.in/public/v2/")
 abstract class WebService {
-  
+  factory WebService(Dio dio, {String baseUrl}) = _WebService;
+
+  @GET("users")
+  Future<List<Users>> getAllUsers();
 }
